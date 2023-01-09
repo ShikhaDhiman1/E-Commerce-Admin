36f28eb9db5b90e1f1e554656072a3273d8e02e0
Shikha Dhiman <dshikha4403@gmail.com>
2023-01-10 00:56:49 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,43 @@
+{/* <script type="module">
+    import {addPr, goToMain, product, brand, price, image} from './script2.mjs';
+</script> */}
+
+var login = document.getElementById('btn-login')
+var email = document.getElementById('InputEmail')
+var passw = document.getElementById('InputPassword')
+
+
+login.addEventListener('click', (e)=>{
+    e.preventDefault()
+    console.log(email.value)
+    console.log(passw.value)
+    // console.log((form.email.value == 'example@gmail.com'))
+    console.log((email.value == 'example@gmail.com')  && (passw.value == 'abc'))
+    if((email.value === "example@gmail.com")  && (passw.value === "abc")){
+
+        document.getElementsByClassName("card-body")[0].innerHTML = "<form id='form-product'><div class='mb-3'><label for='p-name' class='form-label'>Product Name<sup>*</sup></label><input type='text' class='form-control' id='p-name' required></div><div class='mb-3'><label for='p-brand' class='form-label'>Product Brand<sup>*</sup></label><input type='text' class='form-control' id='p-brand' required></div><div class='mb-3'><label for='p-price' class='form-label'>Product Price<sup>*</sup></label><input type='number' class='form-control' id='p-price' required></div><div class='mb-3'><label for='p-image' class='form-label'>Product Image<sup>*</sup></label><input type='file' class='form-control' id='p-image' required></div><br><button type='submit' class='btn btn-primary' id='add-product'>Add Product</button><a href='/main.html' id='go-to-web'>Go to Website</a></form>"
+
+    }
+    else{
+        alert("Wrong Password or Wrong Username! Please, try Again.")
+    }
+    var addPr = document.getElementById('add-product')
+    var goToMain = document.getElementById('go-to-web')
+    var product = document.getElementById('p-name')
+    var brand = document.getElementById('p-brand')
+    var price = document.getElementById('p-price')
+    var image = document.getElementById('p-image')
+    addPr.addEventListener('click', (e)=>{
+        e.preventDefault()
+        console.log(product.value)
+        console.log(brand.value)
+        console.log(price.value)
+        console.log(image.value)
+        console.log(image)
+        localStorage.setItem("product", product.value)
+        localStorage.setItem("brand", brand.value)
+        localStorage.setItem("price", price.value)
+        localStorage.setItem("image", image.value)
+    })
+    console.log(document.getElementById("sec-1"))
+})
---
+++ script2.js
@@ -0,0 +1,29 @@
+var addPr = document.getElementById('add-product')
+var goToMain = document.getElementById('go-to-web')
+var product = document.getElementById('p-name')
+var brand = document.getElementById('p-brand')
+var price = document.getElementById('p-price')
+var image = document.getElementById('p-image')
+var rowProducts = document.getElementsByClassName('row-products')[0]
+
+var productVal = localStorage.getItem("product")
+var brandVal = localStorage.getItem("brand")
+var priceVal = localStorage.getItem("price")
+var imageVal = localStorage.getItem("image")
+
+filename = imageVal.replace(/^.*[\\\/]/,'')
+
+rowProducts.innerHTML +=  `<div class="col-products col-sm-3">
+<a href="#">
+<div class="card">
+    <img src= "images/${filename}" class="card-img-top" alt="sale">
+    <div class="card-body">
+        <h4 class="card-text text-muted product-brand"><small>${brandVal}</small></h4>
+        <h3 class="card-title product-name">${productVal}</h3>
+        <h3 class="price">Price : <span class="text-muted">$ ${priceVal}</span></h3>
+        <button type="button" class="btn btn-outline-dark cart">ADD TO CART</button> 
+        <a href="#"><i class="fa fa-heart-o wishlist" aria-hidden="true"></i></a> <br>
+        <hr>
+        <center><a href="#" class="btn btn-buy btn-primary">Buy Now</a></center>
+    </div>
+</div>`

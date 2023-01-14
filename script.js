15314edc433f21a7ac3ba7f925f1be8a613a1c1f
Shikha Dhiman <dshikha4403@gmail.com>
2023-01-14 11:18:57 +0530

    changed image upload part

--- script.js
+++ script.js
@@ -15,9 +15,10 @@ login.addEventListener('click', (e)=>{
     console.log((email.value == 'example@gmail.com')  && (passw.value == 'abc'))
     if((email.value === "example@gmail.com")  && (passw.value === "abc")){
 
-        document.getElementsByClassName("card-body")[0].innerHTML = "<form id='form-product'><div class='mb-3'><label for='p-name' class='form-label'>Product Name<sup>*</sup></label><input type='text' class='form-control' id='p-name' required></div><div class='mb-3'><label for='p-brand' class='form-label'>Product Brand<sup>*</sup></label><input type='text' class='form-control' id='p-brand' required></div><div class='mb-3'><label for='p-price' class='form-label'>Product Price<sup>*</sup></label><input type='number' class='form-control' id='p-price' required></div><div class='mb-3'><label for='p-image' class='form-label'>Product Image<sup>*</sup></label><input type='file' class='form-control' id='p-image' required></div><br><button type='submit' class='btn btn-primary' id='add-product'>Add Product</button><a href='/index2.html' id='go-to-web'>Go to Website</a></form>"
+        document.getElementsByClassName("card-body")[0].innerHTML = "<form id='form-product'><div class='mb-3'><label for='p-name' class='form-label'>Product Name<sup>*</sup></label><input type='text' class='form-control' id='p-name' required></div><div class='mb-3'><label for='p-brand' class='form-label'>Product Brand<sup>*</sup></label><input type='text' class='form-control' id='p-brand' required></div><div class='mb-3'><label for='p-price' class='form-label'>Product Price<sup>*</sup></label><input type='number' class='form-control' id='p-price' required></div><div class='mb-3'><label for='p-image' class='form-label'>Product Image<sup>*</sup></label><input type='file'  accept='image/*' name='image' class='form-control' id='p-image' required '><p><img id='output' width='200px'></p></div><br><button type='submit' class='btn btn-primary' id='add-product'>Add Product</button><a href='/index2.html' id='go-to-web'>Go to Website</a></form>"
 
     }
+    // style='display: none;
     else{
         alert("Wrong Password or Wrong Username! Please, try Again.")
     }
@@ -27,17 +28,25 @@ login.addEventListener('click', (e)=>{
     var brand = document.getElementById('p-brand')
     var price = document.getElementById('p-price')
     var image = document.getElementById('p-image')
+
     addPr.addEventListener('click', (e)=>{
         e.preventDefault()
-        console.log(product.value)
-        console.log(brand.value)
-        console.log(price.value)
-        console.log(image.value)
-        console.log(image)
+        // console.log(product.value)
+        // console.log(brand.value)
+        // console.log(price.value)
+        // console.log(image.value)
+        
         localStorage.setItem("product", product.value)
         localStorage.setItem("brand", brand.value)
         localStorage.setItem("price", price.value)
-        localStorage.setItem("image", image.value)
+        
+        if(image.files && image.files[0]){
+            var reader = new FileReader();
+            reader.onload = function(e){
+                localStorage.setItem("imgURL", e.target.result)
+            }
+            reader.readAsDataURL(image.files[0]);
+        }
     })
     console.log(document.getElementById("sec-1"))
 })
--- script2.js
+++ script2.js
@@ -9,14 +9,14 @@ var rowProducts = document.getElementsByClassName('row-products')[0]
 var productVal = localStorage.getItem("product")
 var brandVal = localStorage.getItem("brand")
 var priceVal = localStorage.getItem("price")
-var imageVal = localStorage.getItem("image")
+var imageVal = localStorage.getItem("imgURL")
 
-filename = imageVal.replace(/^.*[\\\/]/,'')
+// filename = imageVal.replace(/^.*[\\\/]/,'')
 
 rowProducts.innerHTML +=  `<div class="col-products col-sm-3">
 <a href="#">
 <div class="card">
-    <img src= "images/${filename}" class="card-img-top" alt="sale">
+    <img src= "${imageVal}" class="card-img-top" alt="sale">
     <div class="card-body">
         <h4 class="card-text text-muted product-brand"><small>${brandVal}</small></h4>
         <h3 class="card-title product-name">${productVal}</h3>
